4a0b2c9926f4f86d4f19884e0d4b6f93e68b8b59
Эмир Баймуллин <emirhann205@yandex.ru>
2025-04-29 17:15:15 +0300

    header only for post

--- WebAPI/APIGateway/Middlewares/IdempotencyMiddleware.cs
+++ WebAPI/APIGateway/Middlewares/IdempotencyMiddleware.cs
@@ -1,8 +1,11 @@
-﻿namespace APIGateway.Middlewares;
+﻿using APIGateway.Services;
+
+namespace APIGateway.Middlewares;
 
 public class IdempotencyMiddleware
 {
     private readonly RequestDelegate _next;
+    // private readonly IdempotencyService _idempotencyService;
  
     public IdempotencyMiddleware(RequestDelegate next)
     {
@@ -11,7 +14,14 @@ public class IdempotencyMiddleware
  
     public async Task InvokeAsync(HttpContext context)
     {
-        context.Response.Headers.Add("Ideem", "deeeem");
+        context.Response.Headers.Add("KeyCheck", "Чекнули ключ йоу");
+        if(context.Request.Method == "POST")
+        {
+            if(!context.Request.Headers.ContainsKey("Idempotency-Key"))
+            {
+                context.Response.Headers.Add("Idempotency-Key", "" + Guid.NewGuid().ToString());
+            }
+        }
         await _next(context);
     }
 }
